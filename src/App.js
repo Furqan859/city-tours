f1e0dd7550ff75f8fb63e03c7e3bdb7402a59563
Furqan-Rasool <furqanrasool2@gmail.com>
2022-08-23 17:13:09 +0500

    comit

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import React from 'react';
 import './App.css';
 import Navbar from './components/navbar/Navbar';
 import Tour from './components/Main.js/Tour';
-
-
+import {BrowserRoute , Routes , Route} from 'react-router-dom';
 
 
 function App() {
-  return (<main>
-     <Navbar/>
-     <Tour/>
+  return (<BrowserRoute>
+    <Navbar/>
+    <Routes>
+    
+     <Route path="/tour" element={<Tour/>}/>
+     </Routes>
      
-  </main>
+  </BrowserRoute>
    
     
   );
